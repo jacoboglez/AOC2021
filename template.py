e75193aa0173908cc69de719f25d4eb038bff0de
Jacobo <jacobo.baldonedo@gmail.com>
2021-12-01 14:05:20 +0100

    Initial commit: helper files.

---
+++ template.py
@@ -0,0 +1,31 @@
+
+
+from utils import *
+
+
+def parser(test=False):
+    return Input(DAY, 2021, test=test)
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+    
+
+def main():
+    input = parser()
+    print('RESULTS')
+
+    result_1 = part1(input)
+    print(f'Part 1: {result_1}')
+
+    result_2 = part2(input)
+    print(f'Part 2: {result_2}')
+
+
+if __name__ == "__main__":
+    test(DAY, parser, part1, [], part2, [])
+    main()
